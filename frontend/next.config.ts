afc50122e68b62d85db3856b1c91383a28d0a7e6
Batochir366 <gbataa366@gmail.com>
2025-12-23 19:41:09 +0800

    push

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "randomuser.me",
+      "res.cloudinary.com",
+      "lh3.googleusercontent.com",
+      "img.clerk.com",
+    ],
+  },
 };
 
 export default nextConfig;
